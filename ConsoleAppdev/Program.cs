696577eef0d0834adbc862519f51f727eaeaba27
AndryM <103665742+AndryMinoMax@users.noreply.github.com>
2022-06-07 15:19:58 +0300

    master

--- ConsoleAppdev/Program.cs
+++ ConsoleAppdev/Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Andry!");
-Console.WriteLine("Hello, Andry!");
-Console.WriteLine("Hello, Andry!");
-Console.WriteLine("Hello, Andry!");
-Console.WriteLine("Hello, Andry!");
-Console.WriteLine("Hello, Andry!");
